0b838c5a77a62aa05477a7f49b3a294020d10387
EliasIxD <chrmareli@icloud.com>
2023-11-11 19:40:33 +0100

    Removed Face Detect

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import av
 import cv2
 import numpy
 import time
+import av
 from kivy.app import App
 from kivy.core.window import Window
 from joystick import Joystick
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.behaviors import CoverBehavior
 from kivy.uix.video import Video
+from kivy.core.video import VideoBase
 from tellopy import Tello
 from threading import Thread
-from flask import Response
+from flask import Response, request
 from perfume import route, Perfume
 
 
@@ -20,25 +22,13 @@ class FlaskApp(Perfume):
     def __init__(self, drone=None, **kwargs):
         super(FlaskApp, self).__init__(**kwargs)
         self.drone = drone
-        self.face_detect = False
-
-    def __getattr__(self, name):
-        pass
-
-    @property
-    def face_detect(self):
-        return self.__face_detect
-
-    @face_detect.setter
-    def face_detect(self, val):
-        self.__face_detect = val
 
     @route('/video_feed')
     def video_feed(self):
+        print("Received video feed request from:", request.remote_addr)
+
         def generate():
             try:
-                face_cascade = cv2.CascadeClassifier(
-                    "haarcascade_frontalface_default.xml")
                 retry = 3
                 container = None
                 while container is None and 0 < retry:
@@ -58,35 +48,22 @@ class FlaskApp(Perfume):
                         start_time = time.time()
                         image = numpy.array(frame.to_image())
                         color = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                        if self.face_detect:
-                            gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-                            faces = face_cascade.detectMultiScale(
-                                gray,
-                                scaleFactor=1.1,
-                                minNeighbors=5,
-                                minSize=(30, 30),
-                                flags=cv2.CASCADE_SCALE_IMAGE
-                            )
-                            for (x, y, w, h) in faces:
-                                cv2.rectangle(
-                                    color, (x, y), (x+w, y+h), (0, 255, 0), 2)
                         ret, jpeg = cv2.imencode('.jpg', color)
                         yield (b'--frame\r\n'
                                b'Content-Type: image/jpeg\r\n\r\n' +
                                jpeg.tobytes() +
                                b'\r\n\r\n')
-                        if frame.time_base < 1.0/60:
-                            time_base = 1.0/60
+                        if frame.time_base < 1.0 / 60:
+                            time_base = 1.0 / 60
                         else:
                             time_base = frame.time_base
-                        frame_skip = int((time.time() - start_time)/time_base)
+                        frame_skip = int((time.time() - start_time) / time_base)
             except Exception as ex:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 traceback.print_exception(exc_type, exc_value, exc_traceback)
                 print(ex)
             finally:
                 self.drone.quit()
-                # cv2.destroyAllWindows()
                 App.get_running_app().stop()
 
         return Response(generate(),
@@ -122,7 +99,7 @@ class CoverVideo(CoverBehavior, Video):
 class DragableJoystick(Joystick):
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            self.pos = touch.x-self.width/2, touch.y-self.height/2
+            self.pos = touch.x - self.width / 2, touch.y - self.height / 2
             return super(DragableJoystick, self).on_touch_down(touch)
 
 
@@ -137,22 +114,9 @@ class KivyTelloRoot(FloatLayout):
         self.ids.takeoff.bind(state=self.on_state_takeoff)
         self.ids.rotcw.bind(state=self.on_state_rotcw)
         self.ids.rotccw.bind(state=self.on_state_rotccw)
-        self.ids.facedetect.bind(state=self.on_state_facedetect)
         self.ids.quit.bind(on_press=lambda x: self.stop())
         self.drone = drone
         self.flask_app = flask_app
-        # self.drone.subscribe(self.drone.EVENT_FLIGHT_DATA, self.handler)
-        # self.drone.start_video()
-        # self.drone.subscribe(self.drone.EVENT_VIDEO_FRAME, self.handler)
-
-    # def handler(self, event, sender, data, **args):
-        # drone = sender
-        # if event is self.drone.EVENT_FLIGHT_DATA:
-        #     print(data)
-        # # elif event is self.drone.EVENT_VIDEO_FRAME:
-        # #     pass
-        # # else:
-        # #     rint(('event="%s" data=%s' % (event.getname(), str(data))))
 
     def on_state_takeoff(self, instance, value):
         if value == 'down':
@@ -178,22 +142,10 @@ class KivyTelloRoot(FloatLayout):
             print('stop ccw')
             self.drone.counter_clockwise(0)
 
-    def on_state_facedetect(self, instance, value):
-        if value == 'down':
-            print('start fd')
-            self.flask_app.face_detect = True
-        else:
-            print('stop fd')
-            self.flask_app.face_detect = False
-
     def on_pad_left(self, instance, value):
         x, y = value
         self.stick_data[IDX_YAW] = x
         self.stick_data[IDX_THR] = y
-        # self.ids.pad_left.ids.label.text = \
-        #     'THR: {0:f}\n' \
-        #     'YAW: {1:f}'.format(self.stick_data[IDX_THR],
-        #                         self.stick_data[IDX_YAW])
         self.drone.set_throttle(self.stick_data[IDX_THR])
         self.drone.set_yaw(self.stick_data[IDX_YAW])
 
@@ -201,10 +153,6 @@ class KivyTelloRoot(FloatLayout):
         x, y = value
         self.stick_data[IDX_ROLL] = x
         self.stick_data[IDX_PITCH] = y
-        # self.ids.pad_right.ids.label.text = \
-        #     'ROLL: {0:f}\n' \
-        #     'PITCH: {1:f}'.format(self.stick_data[IDX_ROLL],
-        #                           self.stick_data[IDX_PITCH])
         self.drone.set_roll(self.stick_data[IDX_ROLL])
         self.drone.set_pitch(self.stick_data[IDX_PITCH])
 
@@ -236,11 +184,10 @@ if __name__ in ('__main__', '__android__'):
         drone.wait_for_connection(60.0)
         flask_app = FlaskApp(drone=drone)
         t = Thread(target=start_flask_app, args=(flask_app,))
-        t.setDaemon(True)
+        t.daemon = True
         t.start()
         KivyTelloApp(drone=drone, flask_app=flask_app).run()
     except Exception as ex:
         print(ex)
         drone.quit()
         Window.close()
-        # exit(1)
--- set_env.cmd
+++
@@ -1,3 +0,0 @@
-set INCLUDE=%INCLUDE%;c:\ffmpeg\include
-set LIB=%LIB%;c:\ffmpeg\lib
-set PATH=%PATH%;c:\ffmpeg\bin

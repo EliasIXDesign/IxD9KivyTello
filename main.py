6c3ff451922676b6c1224c1e266529ddc6d07d21
EliasIxD <chrmareli@icloud.com>
2023-11-17 15:24:18 +0100

    Added mission structure with missions

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.behaviors import CoverBehavior
 from kivy.uix.video import Video
 from kivy.core.video import VideoBase
+from kivy.uix.screenmanager import ScreenManager, Screen
 from djitellopy import Tello
 from threading import Thread
 from flask import Response, request
@@ -127,14 +128,20 @@ class KivyTelloApp(App):
     def build(self):
         # Example usage:
         initial_button_texts = ["Dragon", "Fire", "Cops and Robbers"]
-        MainScreen(mission_callback, initial_button_texts, name="main")
 
+        # Create an instance of MainScreen
+        main_screen = MainScreen(self.mission_callback, initial_button_texts, name="main")
+
+        # Create ScreenManager and add MainScreen instance to it
         sm = ScreenManager()
-        sm.add_widget(MainScreen)
+        sm.add_widget(main_screen)
 
+        # Add MissionScreens to ScreenManager
         for i in range(1, 3):
             sm.add_widget(MissionScreen(i, self.mission_callback, name=f"mission{i}"))
-        return KivyTelloRoot(drone=self.drone, flask_app=self.flask_app)
+
+        # Return KivyTelloRoot with ScreenManager as a child
+        return KivyTelloRoot(drone=self.drone, flask_app=self.flask_app, sm=sm)
 
     def on_pause(self):
         return True
@@ -148,11 +155,75 @@ class MissionScreen(Screen):
         super(MissionScreen, self).__init__(**kwargs)
         self.mission_number = mission_number
         self.mission_callback = mission_callback
+        self.setup_mission()
+        self.buttons = []  # To store references to created buttons
+        # Clear existing widgets
+        self.clear_widgets()
+        # Set up the video feed as a kivy widget
+        self.cover_video = CoverVideo()
+        self.add_widget(self.cover_video)
 
-    def on_enter(self, *args):
-        self.show_mission()
+    def setup_mission(self):
+        # Adds the video widget to all missions, if clear all widgets is not called
+        self.add_widget(self.cover_video)
 
-    def show_mission(self):
+        if self.mission_number == 1:
+            self.setup_mission_1()
+        elif self.mission_number == 2:
+            self.setup_mission_2()
+        elif self.mission_number == 3:
+            self.setup_mission_3()
+
+    def setup_mission_1(self):
+        # Shows button to start mission after 5 seconds
+        Clock.schedule_once(self.show_first_button, 5)
+
+    def show_first_button(self, dt):
+        # Create a button with custom text
+        button_1 = Button(text="Click me!", on_press=self.button_1_clicked, pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.add_widget(button_1)
+        self.buttons.append(button_1)  # Store the reference to the button
+
+    def button_1_clicked(self, instance):
+        # Handle button click action (send command to the drone, etc.)
+        self.drone_command_1()
+
+        # Clear only the first button, i.e. the one that was clicked and is in pos 0 of the list
+        self.clear_specific_button(self.buttons[0])
+
+        # After the action is done, schedule the appearance of the second button after 3 seconds
+        Clock.schedule_once(self.show_second_button, 3)
+
+    def show_second_button(self, dt):
+        # Create a button with custom text
+        button_2 = Button(text="Click me again!", on_press=self.button_2_clicked,
+                          pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.add_widget(button_2)
+        self.buttons.append(button_2)  # Store the reference to the button
+
+    def button_2_clicked(self, instance):
+        # Handle button click action (send another command to the drone, etc.)
+        self.drone_command_2()
+
+        # Clear only the second button
+        self.clear_specific_button(self.buttons[1])
+
+        # After the action is done, schedule the appearance of the final button after 3 seconds
+        Clock.schedule_once(self.show_final_button, 3)
+
+    def show_final_button(self, dt):
+        # Create a button with custom text
+        final_button = Button(text="Return to Main Screen", on_press=self.return_to_main_screen,
+                              pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.add_widget(final_button)
+        self.buttons.append(final_button)  # Store the reference to the button
+
+    def clear_specific_button(self, button):
+        # Clear only the specified button
+        self.remove_widget(button)
+        self.buttons.remove(button)
+
+    def setup_mission_2(self):
         mission_successful = self.mission_callback(self.mission_number)
         video_path = "successful_mission.mp4" if mission_successful else "failed_mission.mp4"
 
@@ -161,6 +232,19 @@ class MissionScreen(Screen):
         self.add_widget(cover_video)
 
         Clock.schedule_once(self.return_to_main_screen, 5)
+
+    def setup_mission_3(self):
+        mission_successful = self.mission_callback(self.mission_number)
+        video_path = "successful_mission.mp4" if mission_successful else "failed_mission.mp4"
+
+        # Create CoverVideo dynamically
+        cover_video = CoverVideo(source='http://127.0.0.1:30660/video_feed', state='play', options={'eos': 'loop'})
+        self.add_widget(cover_video)
+
+        Clock.schedule_once(self.return_to_main_screen, 5)
+
+    def on_enter(self, *args):
+        self.setup_mission()
 
     def return_to_main_screen(self, dt):
         self.manager.current = "main"
@@ -188,10 +272,6 @@ class MainScreen(Screen):
         self.clear_widgets()
         new_button = Button(text="New Mission", on_press=self.create_buttons)
         self.add_widget(new_button)
-
-
-
-
 
 
 if __name__ in ('__main__', '__android__'):
